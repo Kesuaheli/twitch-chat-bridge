Kesuaheli/twitch-chat

feat(api)!: switch underlying lib to twitch4j
dropping pircbotx and now using twitch4j

src/main/java/eu/pabl/twitchchat/twitch_integration/Bot.java
@@ -1,95 +1,79 @@
 package eu.pabl.twitchchat.twitch_integration;
 
-import com.google.common.collect.ImmutableMap;
 import eu.pabl.twitchchat.config.ModConfig;
 import java.awt.Color;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.net.ssl.SSLSocketFactory;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
-import org.pircbotx.Channel;
-import org.pircbotx.Configuration;
-import org.pircbotx.PircBotX;
-import org.pircbotx.User;
-import org.pircbotx.cap.EnableCapHandler;
-import org.pircbotx.exception.IrcException;
-import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.events.*;
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
+import com.github.philippheuer.events4j.core.EventManager;
+import com.github.twitch4j.TwitchClient;
+import com.github.twitch4j.TwitchClientBuilder;
+import com.github.twitch4j.chat.events.channel.*;
+import com.github.twitch4j.common.events.domain.EventUser;
 import eu.pabl.twitchchat.TwitchChatMod;
 
-public class Bot extends ListenerAdapter {
-  private final PircBotX ircBot;
+public class Bot {
+  private TwitchClient twitchClient;
+  private final TwitchClientBuilder twitchClientBuilder;
   private final String username;
   private String channel;
   private ExecutorService myExecutor;
   private HashMap<String, TextColor> formattingColorCache; // Map of usernames to colors to keep consistency with usernames and colors
+  private boolean isConnected;
 
   public Bot(String username, String oauthKey, String channel) {
     this.channel = channel.toLowerCase();
     this.username = username.toLowerCase();
     formattingColorCache = new HashMap<>();
 
-    Configuration.Builder builder = new Configuration.Builder()
-        .setAutoNickChange(false) //Twitch doesn't support multiple users
-        .setOnJoinWhoEnabled(false) //Twitch doesn't support WHO command
-        .setEncoding(StandardCharsets.UTF_8) // Use UTF-8 on Windows.
-        .setCapEnabled(true)
-        .addCapHandler(new EnableCapHandler("twitch.tv/membership")) //Twitch by default doesn't send JOIN, PART, and NAMES unless you request it, see https://dev.twitch.tv/docs/irc/guide/#twitch-irc-capabilities
-        .addCapHandler(new EnableCapHandler("twitch.tv/tags"))
-        .addCapHandler(new EnableCapHandler("twitch.tv/commands"))
+    OAuth2Credential credential = new OAuth2Credential("twitch", oauthKey);
+    twitchClientBuilder = TwitchClientBuilder.builder()
+        .withChatAccount(credential)
+        .withEnableChat(true)
+        .withEnableHelix(true)
+        ;
 
-        .addServer("irc.chat.twitch.tv", 6697)
-        .setSocketFactory(SSLSocketFactory.getDefault())
-        .setName(this.username)
-        .setServerPassword(oauthKey);
-
-    if (!channel.equals("")) {
-       builder.addAutoJoinChannel("#" + this.channel);
-    }
-
-    Configuration config = builder.addListener(this)
-        .setAutoSplitMessage(false)
-        .buildConfiguration();
-
-    this.ircBot = new PircBotX(config);
     this.myExecutor = Executors.newCachedThreadPool();
   }
 
   public void start() {
     System.out.println("TWITCH BOT STARTED");
     myExecutor.execute(() -> {
-      try {
-        ircBot.startBot();
-      } catch (IOException | IrcException e) {
-        e.printStackTrace();
-      }
+      isConnected = true;
+      twitchClient = twitchClientBuilder.build();
+      EventManager eventManager = twitchClient.getEventManager();
+      eventManager.onEvent(ChannelMessageEvent.class, this::onMessage);
+      eventManager.onEvent(UserStateEvent.class, this::onUserstate);
+      eventManager.onEvent(ChannelNoticeEvent.class, this::onNotice);
+      eventManager.onEvent(ChannelLeaveEvent.class, this::onDisconnect);
+      eventManager.onEvent(ChannelMessageActionEvent.class, this::onAction);
+      eventManager.onEvent(GlobalUserStateEvent.class, this::onJoin);
     });
   }
 
   public void stop() {
-    ircBot.stopBotReconnect();
-    ircBot.close();
+    isConnected = false;
+    twitchClient.close();
   }
 
   public boolean isConnected() {
-    return ircBot.isConnected();
+    return isConnected;
   }
 
-  @Override
-  public void onMessage(MessageEvent event) throws Exception {
+  public void onMessage(ChannelMessageEvent event) {
     String message = event.getMessage();
     System.out.println("TWITCH MESSAGE: " + message);
-    User user = event.getUser();
+    EventUser user = event.getUser();
     if (user != null) {
-      ImmutableMap<String, String> v3Tags = event.getV3Tags();
+      Map<String, String> v3Tags = event.getMessageEvent().getTags();
       if (v3Tags != null) {
-        String nick = user.getNick();
+        String nick = user.getName();
         if (!ModConfig.getConfig().getIgnoreList().contains(nick)) {
           String colorTag = v3Tags.get("color");
           TextColor formattingColor;
@@ -121,56 +105,37 @@ public class Bot extends ListenerAdapter {
     }
   }
 
-  @Override
-  public void onUnknown(UnknownEvent event) throws Exception {
-    switch (event.getCommand()) {
-      case "USERSTATE" -> {
+  public void onUserstate(UserStateEvent event) {
         // Info about our user. More at https://dev.twitch.tv/docs/irc/commands/#userstate
         // Set our correct colour :).
-        String colorTag = event.getTags().get("color");
+        String colorTag = event.getColor().orElse(null);
         if (colorTag != null) {
           Color userColor = Color.decode(colorTag);
           TextColor formattingColor = TextColor.fromRgb(userColor.getRGB());
 
           putFormattingColor(getUsername(), formattingColor);
         }
-      }
-      default -> {
-        System.out.println("UNKNOWN TWITCH EVENT: " + event.toString());
-      }
-    }
   }
 
-  @Override
-  public void onNotice(NoticeEvent event) {
+  public void onNotice(ChannelNoticeEvent event) {
     System.out.println("TWITCH NOTICE: " + event.toString());
-    TwitchChatMod.addNotification(Text.literal(event.getNotice()));
+    TwitchChatMod.addNotification(Text.literal(event.getMessage()));
   }
 
-  @Override
-  public void onKick(KickEvent event) {
-    System.out.println("TWITCH KICK: " + event.toString());
-    String message = event.getReason();
-    TwitchChatMod.addNotification(Text.translatable("text.twitchchat.bot.kicked", message));
-  }
 
-  @Override
-  public void onDisconnect(DisconnectEvent event) throws Exception {
-    super.onDisconnect(event);
+  public void onDisconnect(ChannelLeaveEvent event) {
     System.out.println("TWITCH DISCONNECT: " + event.toString());
-    Exception disconnectException = event.getDisconnectException();
   }
 
   // Handle /me
-  @Override
-  public void onAction(ActionEvent event) throws Exception {
-    User user = event.getUser();
+  public void onAction(ChannelMessageActionEvent event) {
+    EventUser user = event.getUser();
 
     if (user != null) {
-      String nick = user.getNick();
+      String nick = user.getName();
 
       if (!ModConfig.getConfig().getIgnoreList().contains(nick.toLowerCase())) {
-        String formattedTime = TwitchChatMod.formatTMISentTimestamp(event.getTimestamp());
+        String formattedTime = TwitchChatMod.formatDateTwitch(event.getFiredAt().getTime());
 
         TextColor formattingColor;
         if (isFormattingColorCached(nick)) {
@@ -187,27 +152,16 @@ public class Bot extends ListenerAdapter {
     }
   }
 
-  Channel currentChannel;
-  @Override
-  public void onJoin(JoinEvent event) throws Exception {
-    super.onJoin(event);
-    Channel channel = event.getChannel();
-     if (currentChannel == null || !currentChannel.equals(channel)) {
-      TwitchChatMod.addNotification(Text.translatable("text.twitchchat.bot.connected", this.channel));
-      currentChannel = channel;
-    }
+  public void onJoin(GlobalUserStateEvent event) {
+    event.getMessageEvent().getChannelName().ifPresent(channel -> {
+      event.getColor().ifPresent(color -> putFormattingColor(event.getDisplayName().orElse(""), color));
+      TwitchChatMod.addNotification(Text.translatable("text.twitchchat.bot.connected", channel));
+    });
   }
 
-  /**
-   * We MUST respond to this or else we will get kicked
-   */
-  @Override
-  public void onPing(PingEvent event) {
-    ircBot.sendRaw().rawLineNow(String.format("PONG %s\r\n", event.getPingValue()));
-  }
 
   public void sendMessage(String message) {
-    ircBot.sendIRC().message("#" + this.channel, message);
+    twitchClient.getChat().sendMessage(this.channel, message);
   }
 
   public String getUsername() {
@@ -215,7 +169,11 @@ public class Bot extends ListenerAdapter {
   }
 
   public void putFormattingColor(String nick, TextColor color) {
+    if (nick == null || nick.equals("") || color == null) return;
     formattingColorCache.put(nick.toLowerCase(), color);
+  }
+  public void putFormattingColor(String nick, String color) {
+    this.putFormattingColor(nick, TextColor.fromRgb(Integer.valueOf(color.replace("#", ""), 16)));
   }
   public TextColor getFormattingColor(String nick) {
     return formattingColorCache.get(nick.toLowerCase());
@@ -227,11 +185,10 @@ public class Bot extends ListenerAdapter {
   public void joinChannel(String channel) {
     String oldChannel = this.channel;
     this.channel = channel.toLowerCase();
-    if (ircBot.isConnected()) {
+    if (this.isConnected()) {
       myExecutor.execute(() -> {
-        ircBot.sendRaw().rawLine("PART #" + oldChannel); // Leave the channel
-        ircBot.sendIRC().joinChannel("#" + this.channel); // Join the new channel
-        ircBot.sendCAP().request("twitch.tv/membership", "twitch.tv/tags", "twitch.tv/commands"); // Ask for capabilities
+        twitchClient.getChat().leaveChannel(oldChannel); // Leave the channel
+        twitchClient.getChat().joinChannel(this.channel); // Join the new channel
       });
     }
   }

Kesuaheli/twitch-chat-bridge

src/main/java/eu/pabl/twitchchat/twitch_integration/Bot.java
@@ -1,95 +1,83 @@
 package eu.pabl.twitchchat.twitch_integration;
 
-import com.google.common.collect.ImmutableMap;
 import eu.pabl.twitchchat.config.ModConfig;
 import java.awt.Color;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.net.ssl.SSLSocketFactory;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
-import org.pircbotx.Channel;
-import org.pircbotx.Configuration;
-import org.pircbotx.PircBotX;
-import org.pircbotx.User;
-import org.pircbotx.cap.EnableCapHandler;
-import org.pircbotx.exception.IrcException;
-import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.events.*;
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
+import com.github.philippheuer.events4j.core.EventManager;
+import com.github.twitch4j.TwitchClient;
+import com.github.twitch4j.TwitchClientBuilder;
+import com.github.twitch4j.chat.events.channel.*;
+import com.github.twitch4j.common.events.domain.EventUser;
 import eu.pabl.twitchchat.TwitchChatMod;
 
-public class Bot extends ListenerAdapter {
-  private final PircBotX ircBot;
+public class Bot {
+  private TwitchClient twitchClient;
+  private final TwitchClientBuilder twitchClientBuilder;
   private final String username;
   private String channel;
   private ExecutorService myExecutor;
   private HashMap<String, TextColor> formattingColorCache; // Map of usernames to colors to keep consistency with usernames and colors
+  private boolean isConnected;
 
   public Bot(String username, String oauthKey, String channel) {
     this.channel = channel.toLowerCase();
     this.username = username.toLowerCase();
     formattingColorCache = new HashMap<>();
 
-    Configuration.Builder builder = new Configuration.Builder()
-        .setAutoNickChange(false) //Twitch doesn't support multiple users
-        .setOnJoinWhoEnabled(false) //Twitch doesn't support WHO command
-        .setEncoding(StandardCharsets.UTF_8) // Use UTF-8 on Windows.
-        .setCapEnabled(true)
-        .addCapHandler(new EnableCapHandler("twitch.tv/membership")) //Twitch by default doesn't send JOIN, PART, and NAMES unless you request it, see https://dev.twitch.tv/docs/irc/guide/#twitch-irc-capabilities
-        .addCapHandler(new EnableCapHandler("twitch.tv/tags"))
-        .addCapHandler(new EnableCapHandler("twitch.tv/commands"))
+    OAuth2Credential credential = new OAuth2Credential("twitch", oauthKey);
+    twitchClientBuilder = TwitchClientBuilder.builder()
+        .withChatAccount(credential)
+        .withEnableChat(true)
+        .withEnableHelix(true)
+        ;
 
-        .addServer("irc.chat.twitch.tv", 6697)
-        .setSocketFactory(SSLSocketFactory.getDefault())
-        .setName(this.username)
-        .setServerPassword(oauthKey);
-
-    if (!channel.equals("")) {
-       builder.addAutoJoinChannel("#" + this.channel);
-    }
-
-    Configuration config = builder.addListener(this)
-        .setAutoSplitMessage(false)
-        .buildConfiguration();
-
-    this.ircBot = new PircBotX(config);
     this.myExecutor = Executors.newCachedThreadPool();
   }
 
   public void start() {
     System.out.println("TWITCH BOT STARTED");
     myExecutor.execute(() -> {
-      try {
-        ircBot.startBot();
-      } catch (IOException | IrcException e) {
-        e.printStackTrace();
+      isConnected = true;
+      twitchClient = twitchClientBuilder.build();
+      EventManager eventManager = twitchClient.getEventManager();
+      eventManager.onEvent(ChannelMessageEvent.class, this::onMessage);
+      eventManager.onEvent(GlobalUserStateEvent.class, this::onConnect);
+      eventManager.onEvent(ChannelNoticeEvent.class, this::onNotice);
+      eventManager.onEvent(ChannelLeaveEvent.class, this::onDisconnect);
+      eventManager.onEvent(ChannelMessageActionEvent.class, this::onAction);
+      eventManager.onEvent(ChannelStateEvent.class, this::onJoin);
+
+      if (!Objects.equals(this.channel, "")) {
+        joinChannel(channel);
       }
     });
   }
 
   public void stop() {
-    ircBot.stopBotReconnect();
-    ircBot.close();
+    isConnected = false;
+    twitchClient.close();
   }
 
   public boolean isConnected() {
-    return ircBot.isConnected();
+    return isConnected;
   }
 
-  @Override
-  public void onMessage(MessageEvent event) throws Exception {
+  public void onMessage(ChannelMessageEvent event) {
     String message = event.getMessage();
     System.out.println("TWITCH MESSAGE: " + message);
-    User user = event.getUser();
+    EventUser user = event.getUser();
     if (user != null) {
-      ImmutableMap<String, String> v3Tags = event.getV3Tags();
+      Map<String, String> v3Tags = event.getMessageEvent().getTags();
       if (v3Tags != null) {
-        String nick = user.getNick();
+        String nick = user.getName();
         if (!ModConfig.getConfig().getIgnoreList().contains(nick)) {
           String colorTag = v3Tags.get("color");
           TextColor formattingColor;
@@ -121,56 +109,37 @@ public class Bot extends ListenerAdapter {
     }
   }
 
-  @Override
-  public void onUnknown(UnknownEvent event) throws Exception {
-    switch (event.getCommand()) {
-      case "USERSTATE" -> {
+  public void onConnect(GlobalUserStateEvent event) {
         // Info about our user. More at https://dev.twitch.tv/docs/irc/commands/#userstate
         // Set our correct colour :).
-        String colorTag = event.getTags().get("color");
+        String colorTag = event.getColor().orElse(null);
         if (colorTag != null) {
           Color userColor = Color.decode(colorTag);
           TextColor formattingColor = TextColor.fromRgb(userColor.getRGB());
 
           putFormattingColor(getUsername(), formattingColor);
         }
-      }
-      default -> {
-        System.out.println("UNKNOWN TWITCH EVENT: " + event.toString());
-      }
-    }
   }
 
-  @Override
-  public void onNotice(NoticeEvent event) {
+  public void onNotice(ChannelNoticeEvent event) {
     System.out.println("TWITCH NOTICE: " + event.toString());
-    TwitchChatMod.addNotification(Text.literal(event.getNotice()));
+    TwitchChatMod.addNotification(Text.literal(event.getMessage()));
   }
 
-  @Override
-  public void onKick(KickEvent event) {
-    System.out.println("TWITCH KICK: " + event.toString());
-    String message = event.getReason();
-    TwitchChatMod.addNotification(Text.translatable("text.twitchchat.bot.kicked", message));
-  }
 
-  @Override
-  public void onDisconnect(DisconnectEvent event) throws Exception {
-    super.onDisconnect(event);
+  public void onDisconnect(ChannelLeaveEvent event) {
     System.out.println("TWITCH DISCONNECT: " + event.toString());
-    Exception disconnectException = event.getDisconnectException();
   }
 
   // Handle /me
-  @Override
-  public void onAction(ActionEvent event) throws Exception {
-    User user = event.getUser();
+  public void onAction(ChannelMessageActionEvent event) {
+    EventUser user = event.getUser();
 
     if (user != null) {
-      String nick = user.getNick();
+      String nick = user.getName();
 
       if (!ModConfig.getConfig().getIgnoreList().contains(nick.toLowerCase())) {
-        String formattedTime = TwitchChatMod.formatTMISentTimestamp(event.getTimestamp());
+        String formattedTime = TwitchChatMod.formatDateTwitch(event.getFiredAt().getTime());
 
         TextColor formattingColor;
         if (isFormattingColorCached(nick)) {
@@ -187,27 +156,17 @@ public class Bot extends ListenerAdapter {
     }
   }
 
-  Channel currentChannel;
-  @Override
-  public void onJoin(JoinEvent event) throws Exception {
-    super.onJoin(event);
-    Channel channel = event.getChannel();
+  String currentChannel;
+  public void onJoin(ChannelStateEvent event) {
+    String channel = event.getChannel().getName();
      if (currentChannel == null || !currentChannel.equals(channel)) {
       TwitchChatMod.addNotification(Text.translatable("text.twitchchat.bot.connected", this.channel));
       currentChannel = channel;
     }
   }
 
-  /**
-   * We MUST respond to this or else we will get kicked
-   */
-  @Override
-  public void onPing(PingEvent event) {
-    ircBot.sendRaw().rawLineNow(String.format("PONG %s\r\n", event.getPingValue()));
-  }
-
   public void sendMessage(String message) {
-    ircBot.sendIRC().message("#" + this.channel, message);
+    twitchClient.getChat().sendMessage(this.channel, message);
   }
 
   public String getUsername() {
@@ -215,7 +174,11 @@ public class Bot extends ListenerAdapter {
   }
 
   public void putFormattingColor(String nick, TextColor color) {
+    if (nick == null || nick.equals("") || color == null) return;
     formattingColorCache.put(nick.toLowerCase(), color);
+  }
+  public void putFormattingColor(String nick, String color) {
+    this.putFormattingColor(nick, TextColor.fromRgb(Integer.valueOf(color.replace("#", ""), 16)));
   }
   public TextColor getFormattingColor(String nick) {
     return formattingColorCache.get(nick.toLowerCase());
@@ -227,11 +190,10 @@ public class Bot extends ListenerAdapter {
   public void joinChannel(String channel) {
     String oldChannel = this.channel;
     this.channel = channel.toLowerCase();
-    if (ircBot.isConnected()) {
+    if (this.isConnected()) {
       myExecutor.execute(() -> {
-        ircBot.sendRaw().rawLine("PART #" + oldChannel); // Leave the channel
-        ircBot.sendIRC().joinChannel("#" + this.channel); // Join the new channel
-        ircBot.sendCAP().request("twitch.tv/membership", "twitch.tv/tags", "twitch.tv/commands"); // Ask for capabilities
+        twitchClient.getChat().leaveChannel(oldChannel); // Leave the channel
+        twitchClient.getChat().joinChannel(this.channel); // Join the new channel
       });
     }
   }