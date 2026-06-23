dbf683cdd474d3b336ef3699cc01c2766495d2b6
Kesu <kesuaheligame@gmail.com>
2026-06-23 23:11:22 +0200

    feat: update to MC 26.2

--- src/main/java/de/kesuaheli/twitchchatbridge/TwitchChatMod.java
+++ src/main/java/de/kesuaheli/twitchchatbridge/TwitchChatMod.java
@@ -65,9 +65,9 @@ public class TwitchChatMod implements ModInitializer {
     }
 
     if (RenderSystem.isOnRenderThread()) {
-      Minecraft.getInstance().gui.getChat().addClientSystemMessage(message);
+      Minecraft.getInstance().gui.hud.getChat().addClientSystemMessage(message);
     } else {
-      Minecraft.getInstance().executeIfPossible(() -> Minecraft.getInstance().gui.getChat().addClientSystemMessage(message));
+      Minecraft.getInstance().executeIfPossible(() -> Minecraft.getInstance().gui.hud.getChat().addClientSystemMessage(message));
     }
   }
 
@@ -108,9 +108,9 @@ public class TwitchChatMod implements ModInitializer {
     }
 
     if (RenderSystem.isOnRenderThread()) {
-      Minecraft.getInstance().gui.getChat().addClientSystemMessage(message);
+      Minecraft.getInstance().gui.hud.getChat().addClientSystemMessage(message);
     } else {
-      Minecraft.getInstance().executeIfPossible(() -> Minecraft.getInstance().gui.getChat().addClientSystemMessage(message));
+      Minecraft.getInstance().executeIfPossible(() -> Minecraft.getInstance().gui.hud.getChat().addClientSystemMessage(message));
     }
   }
 }
